8ec09a555894df0693331d65234afc9e038e96bd
Avi Desai <avidesai2@gmail.com>
2022-12-05 00:39:41 -0800

    restarted website

---
+++ bet-site/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href = "/">butter.</a>
+      </header>
+      <main>
+        list bets
+      </main>
+    </div>
+  );
+}
+
+export default App;
