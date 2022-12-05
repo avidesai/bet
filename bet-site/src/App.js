aedad609bfd7dbd631c45dfc104ca019cd7ecb11
Avi Desai <avidesai2@gmail.com>
2022-12-05 02:08:32 -0800

    basic UI elements are created

--- bet-site/src/App.js
+++ bet-site/src/App.js
@@ -1,13 +1,34 @@
-import './App.css';
+import data from "./data";
 
 function App() {
   return (
     <div>
       <header>
-        <a href = "/">butter.</a>
+        <a href = "/"><h1>butter</h1></a>
+        <p>bet on real events happening soon.</p>
       </header>
       <main>
-        list bets
+        <div className = "bet_cards">
+          {data.active_bets.map((bet) => (
+              <div className = "bet_card">
+                <div className = 'card_top_half'>
+                  <p>{bet.time_left} hours left</p>
+                  <h2>{bet.name}</h2>
+                </div>
+                <div className = 'card_bottom_half'>
+                  <div className = 'price_text'>
+                    <h3>${bet.price.toLocaleString('en', {useGrouping:true})}</h3>
+                    <p>Pot size</p>
+                  </div>
+                  <div className = 'button_section'> 
+                    <button name = "firstbet" type = "submit"><img src = {bet.choice_one_image} alt = {bet.choice_one}/>   {bet.percent_one}%</button>
+                    <button name = "secondbet" type = "submit"><img src = {bet.choice_two_image} alt = {bet.choice_two}/>   {bet.percent_two}%</button>
+                  </div>
+                </div>
+              </div>
+            ))
+          }
+        </div>
       </main>
     </div>
   );
---
+++ bet-site/src/data.js
@@ -0,0 +1,44 @@
+const data = {
+    active_bets: [
+        {
+            name: "Who Will Win the World Cup?",
+            slug: 'world-cup-2022',
+            category: 'sports',
+            price: 125000,
+            time_left: 6,
+            choice_one: "Netherlands",
+            choice_one_image: '/images/netherlands.png',
+            percent_one: 77,
+            choice_two: "England",
+            choice_two_image: '/images/england.png', 
+            percent_two: 23
+        },
+        {
+            name: "Who Will Win the 2024 Election?",
+            slug: 'election-2024',
+            category: 'politics',
+            price: 24500,
+            time_left: 18,
+            choice_one: "Republican",
+            choice_one_image: '/images/republican.png',
+            percent_one: 55,
+            choice_two: "Democrat",
+            choice_two_image: '/images/democrat.jpeg', 
+            percent_two: 45
+        },
+        {
+            name: "Who Will Win the Bachelorette?",
+            slug: 'bachelorette-2022',
+            category: 'entertainment',
+            price: 12000,
+            time_left: 23,
+            choice_one: "Jane",
+            choice_one_image: '/images/moneybag.png',
+            percent_one: 70,
+            choice_two: "Carol",
+            choice_two_image: '/images/moneybag.png', 
+            percent_two: 30
+        },
+    ],
+};
+export default data;
